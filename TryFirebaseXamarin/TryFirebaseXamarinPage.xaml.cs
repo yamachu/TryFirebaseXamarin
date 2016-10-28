5a916e7f8d2bfada662eb0793a6b8e0efe628ece
Yusuke Yamada <yusuke.yamada.t1@dc.tohoku.ac.jp>
2016-10-28 13:03:10 +0900

    Initial commit

---
+++ TryFirebaseXamarin/TryFirebaseXamarinPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace TryFirebaseXamarin
+{
+	public partial class TryFirebaseXamarinPage : ContentPage
+	{
+		public TryFirebaseXamarinPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
