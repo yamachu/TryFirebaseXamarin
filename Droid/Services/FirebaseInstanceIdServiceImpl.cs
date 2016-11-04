ab65553ff262e5c59e6b6e071073b9dd3ef34a3d
Yusuke Yamada <yusuke.yamada.t1@dc.tohoku.ac.jp>
2016-11-04 18:10:37 +0900

    Add cloud messaging for Android

---
+++ Droid/Services/FirebaseInstanceIdServiceImpl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Android.App;
+using Firebase.Iid;
+
+namespace TryFirebaseXamarin.Droid
+{
+	[Service]
+	[IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+	public class FirebaseInstanceIdServiceImpl : FirebaseInstanceIdService
+	{
+		public override void OnTokenRefresh()
+		{
+			// Get updated InstanceID token.
+			var refreshedToken = FirebaseInstanceId.Instance.Token;
+			System.Diagnostics.Debug.WriteLine(refreshedToken);
+		}
+	}
+}
---
+++ Droid/Services/FirebaseMessagingServiceImpl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Support.V4.App;
+using Firebase.Messaging;
+
+namespace TryFirebaseXamarin.Droid
+{
+	[Service]
+	[IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+	public class FirebaseMessagingServiceImpl : FirebaseMessagingService
+	{
+		// application is in the foreground, this method will fire.
+		public override void OnMessageReceived(RemoteMessage message)
+		{
+			System.Diagnostics.Debug.WriteLine(message.GetNotification().Body);
+			showNotification(message);
+		}
+
+		void showNotification(RemoteMessage message) 
+		{
+			var intent = new Intent(this, typeof(MainActivity));
+			intent.AddFlags(ActivityFlags.ClearTop);
+			var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
+
+			var notificationBuilder = new NotificationCompat.Builder(this)
+			                                                .SetSmallIcon(Resource.Drawable.icon)
+			                                                .SetContentTitle(message.GetNotification().Title ?? "from Firebase")
+			                                                .SetContentText(message.GetNotification().Body)
+			                                                .SetAutoCancel(true)
+			                                                .SetContentIntent(pendingIntent);
+
+			var notificationManager = NotificationManager.FromContext(this);
+
+			notificationManager.Notify(0, notificationBuilder.Build());
+		}
+	}
+}
